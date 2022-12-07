e6e37c4601bd063d630f9980c3825c881c6c0112
Jefersson Nathan <malukenho.dev@gmail.com>
2022-12-07 03:50:24 +0100

    DaySix: Refactoring

--- src/main/kotlin/DaySix/Main.kt
+++ src/main/kotlin/DaySix/Main.kt
@@ -5,33 +5,20 @@ import java.lang.RuntimeException
 import kotlin.math.max
 
 fun main() {
+    fun String.rangeIsMarker(index: Int, step: Int): Boolean = step == this.subSequence(max(0, index - step), max(index, step))
+        .chunked(1)
+        .distinct()
+        .count()
 
-    fun solutionWithSteps(step: Int, input: String): Int {
+    fun findMarkerWithStep(step: Int, input: String): Int {
         input.chunked(1).forEachIndexed { index, _ ->
             if (index < step) return@forEachIndexed
-
-            if (input.subSequence(max(0, index - step), max(index, step))
-                    .chunked(1)
-                    .distinct().size == step
-            ) {
-                return index
-            }
+            if (input.rangeIsMarker(index, step)) return index
         }
 
         throw RuntimeException("Couldnt find the number")
     }
 
-    check(7 == solutionWithSteps(4, "mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
-    check(5 == solutionWithSteps(4, "bvwbjplbgvbhsrlpgdmjqwftvncz"))
-    check(6 == solutionWithSteps(4, "nppdvjthqldpwncqszvftbrmjlhg"))
-    check(10 == solutionWithSteps(4, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
-    check(11 == solutionWithSteps(4, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
-    check(1623 == solutionWithSteps(4, readInput(6)))
-
-    check(19 == solutionWithSteps(14, "mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
-    check(23 == solutionWithSteps(14, "bvwbjplbgvbhsrlpgdmjqwftvncz"))
-    check(23 == solutionWithSteps(14, "nppdvjthqldpwncqszvftbrmjlhg"))
-    check(29 == solutionWithSteps(14, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
-    check(26 == solutionWithSteps(14, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
-    check(3774 == solutionWithSteps(14, readInput(6)))
+    check(1623 == findMarkerWithStep(4, readInput(6)))
+    check(3774 == findMarkerWithStep(14, readInput(6)))
 }
