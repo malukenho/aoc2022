7bb4259423118dc1d1853bf70a924e7ebf2640de
Jefersson Nathan <malukenho.dev@gmail.com>
2022-12-14 20:05:55 +0100

    refactor: Day 10

--- src/main/kotlin/com/github/malukenho/aoc2022/Day10.kt
+++ src/main/kotlin/com/github/malukenho/aoc2022/Day10.kt
@@ -1,35 +1,27 @@
 package com.github.malukenho.aoc2022
 
 class Day10(private val list: List<String>) {
-    private val cycles = mutableListOf<Int>()
 
-    fun part1(cycleTick: Int): Int {
+    private val cycles: List<Int> = buildList {
         // The initially signal is 1
-        cycles.add(1)
+        add(1)
         list.map {
-            if (it == "noop") {
-                cycles.add(0)
-            } else {
-                cycles.add(0)
-                cycles.add(it.split(" ").last().toInt())
+            if (it == "noop") add(0) else {
+                add(0); add(it.split(" ").last().toInt())
             }
         }
+    }
 
+    fun part1(cycleTick: Int): Int {
         return cycles.chunked(cycleTick).first().sumOf { it } * cycleTick
     }
 
     fun part2(): String {
-        list.map {
-            if (it == "noop") cycles.add(0) else {
-                cycles.add(0); cycles.add(it.split(" ").last().toInt())
-            }
-        }
-
         fun Int.range() = IntRange(this - 1, this + 1)
         var position = 1
         var stream = ""
 
-        cycles.chunked(40).map {
+        cycles.drop(1).chunked(40).map {
             it.forEachIndexed { index, x ->
                 stream += if (index in position.range()) "#" else "."
                 if ((index + 1) % 40 == 0) stream += "\n"
