8901f6080acaddd419c46527a9869f1b2c1e9aac
Jefersson Nathan <malukenho.dev@gmail.com>
2022-12-15 08:46:44 +0100

    refactor: Day 10 simplify functions

--- src/main/kotlin/com/github/malukenho/aoc2022/Day10.kt
+++ src/main/kotlin/com/github/malukenho/aoc2022/Day10.kt
@@ -3,18 +3,11 @@ package com.github.malukenho.aoc2022
 class Day10(private val list: List<String>) {
 
     private val cycles: List<Int> = buildList {
-        // The initially signal is 1
-        add(1)
-        list.map {
-            if (it == "noop") add(0) else {
-                add(0); add(it.split(" ").last().toInt())
-            }
-        }
+        add(1) // The initially signal is 1
+        list.map { add(0); if (it != "noop") add(it.split(" ").last().toInt()) }
     }
 
-    fun part1(cycleTick: Int): Int {
-        return cycles.chunked(cycleTick).first().sumOf { it } * cycleTick
-    }
+    fun part1(cycleTick: Int): Int = cycles.chunked(cycleTick).first().sumOf { it } * cycleTick
 
     fun part2(): String {
         fun Int.range() = IntRange(this - 1, this + 1)
