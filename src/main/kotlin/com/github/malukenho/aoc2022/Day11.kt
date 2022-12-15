c39a4965af48e2e878c9c5bc1ad63ee97b936137
Jefersson Nathan <malukenho.dev@gmail.com>
2022-12-15 22:41:36 +0100

    refactor: Day 11 to reuse code

--- src/main/kotlin/com/github/malukenho/aoc2022/Day11.kt
+++ src/main/kotlin/com/github/malukenho/aoc2022/Day11.kt
@@ -1,7 +1,6 @@
 package com.github.malukenho.aoc2022
 
-class Day11(private val input: String) {
-
+class Day11(input: String) {
 
     data class Item(var worryLevel: Long)
     data class Test(val divisibleBy: Long, val ifTrue: Int, val ifFalse: Int)
@@ -10,7 +9,7 @@ class Day11(private val input: String) {
         val id: Int,
         var items: List<Item>,
         val operation: String,
-        val test: Test,
+        val test: Test
     )
 
     private val monkeys = input.split("\n\n").map {
@@ -21,54 +20,42 @@ class Day11(private val input: String) {
         var ifTrue = 0
         var ifFalse = 0
         it.lines().forEach { line ->
-            if (line.startsWith("Monkey ")) {
-                monkeyID = line.split(' ').last().trim(':').toInt()
-            }
-            if (line.startsWith("  Starting items: ")) {
-                items = line.replace("  Starting items: ", "")
-                    .split(", ")
-                    .map { Item(it.toLong()) }
-            }
-            if (line.startsWith("  Operation: ")) {
-                operation = line.replace("  Operation: ", "")
-            }
-            if (line.startsWith("  Test: divisible by ")) {
-                divisibleBy = line.replace("  Test: divisible by ", "").toLong()
-            }
-            if (line.startsWith("    If true: throw to monkey ")) {
-                ifTrue = line.replace("    If true: throw to monkey ", "").toInt()
-            }
-            if (line.startsWith("    If false: throw to monkey ")) {
-                ifFalse = line.replace("    If false: throw to monkey ", "").toInt()
+            when {
+                line.startsWith("  Operation: ")                  -> operation = line.replace("  Operation: ", "")
+                line.startsWith("  Test: divisible by ")          -> divisibleBy =
+                    line.replace("  Test: divisible by ", "").toLong()
+
+                line.startsWith("    If true: throw to monkey ")  -> ifTrue =
+                    line.replace("    If true: throw to monkey ", "").toInt()
+
+                line.startsWith("    If false: throw to monkey ") -> ifFalse =
+                    line.replace("    If false: throw to monkey ", "").toInt()
+
+                line.startsWith("Monkey ")                        -> monkeyID = line.split(' ').last().trim(':').toInt()
+                line.startsWith("  Starting items: ")             -> items =
+                    line.replace("  Starting items: ", "").split(", ").map { Item(it.toLong()) }
             }
         }
 
         Monkey(0, monkeyID, items, operation, Test(divisibleBy, ifTrue, ifFalse))
     }
 
-    fun compute(round: Int, transformer: (Long) -> Long): List<Monkey> {
+    private fun calculateMonkeyBusinessLevel(round: Int, transformer: (Long) -> Long): Long {
         var copy = monkeys.toMutableList()
         repeat(round) {
             copy.forEachIndexed { id, monkey ->
                 monkey.items.forEach { item ->
-
                     monkey.monkeyBussiness++
-
-                    // Inspect
                     var (operator, operation, operand) = monkey.operation.split(" ").drop(2)
 
-                    if (operator == "old") {
-                        operator = item.worryLevel.toString()
-                    }
-                    if (operand == "old") {
-                        operand = item.worryLevel.toString()
-                    }
+                    operator = if (operator == "old") item.worryLevel.toString() else operator
+                    operand = if (operand == "old") item.worryLevel.toString() else operand
+
                     item.worryLevel = when (operation) {
-                        "+" -> operand.toLong().plus(operator.toLong())
-                        "*" -> operand.toLong().times(operator.toLong())
+                        "+"  -> operand.toLong().plus(operator.toLong())
+                        "*"  -> operand.toLong().times(operator.toLong())
                         else -> error("$operation not supported")
                     }
-
 
                     item.worryLevel %= copy.map { it.test.divisibleBy.toLong() }.reduce(Long::times)
                     item.worryLevel = transformer(item.worryLevel)
@@ -77,23 +64,12 @@ class Day11(private val input: String) {
                 }
             }
         }
-        return copy
-    }
-
-    fun part1(round: Int): Long {
-        val result = compute(round) { x -> x / 3L }
-
-        val mostActive = result.map { x -> x.monkeyBussiness }.sortedDescending().take(2)
+        val mostActive = copy.map { x -> x.monkeyBussiness }.sortedDescending().take(2)
 
         return mostActive[0] * mostActive[1]
     }
 
-    fun part2(round: Int): Long {
+    fun part1(round: Int): Long = calculateMonkeyBusinessLevel(round) { x -> x / 3L }
 
-        val result = compute(round) { x -> x }
-
-        val mostActive = result.map { x -> x.monkeyBussiness }.sortedDescending().take(2)
-
-        return mostActive[0] * mostActive[1]
-    }
+    fun part2(round: Int): Long = calculateMonkeyBusinessLevel(round) { x -> x }
 }
