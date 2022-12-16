df9d3b18c38b3cb81c0f495619712841e1cbffe3
Jefersson Nathan <malukenho.dev@gmail.com>
2022-12-16 13:25:02 +0100

    refactor: Day 11 style adjustments

--- src/main/kotlin/com/github/malukenho/aoc2022/Day11.kt
+++ src/main/kotlin/com/github/malukenho/aoc2022/Day11.kt
@@ -4,44 +4,27 @@ class Day11(input: String) {
 
     data class Item(var worryLevel: Long)
     data class Test(val divisibleBy: Long, val ifTrue: Int, val ifFalse: Int)
-    data class Monkey(
-        var monkeyBussiness: Long,
-        val id: Int,
-        var items: List<Item>,
-        val operation: String,
-        val test: Test
-    )
+    data class Monkey(var monkeyBussiness: Long, val id: Int, var items: List<Item>, val operation: String, val test: Test)
 
     private val monkeys = input.split("\n\n").map {
-        var monkeyID = 0
+        var (monkeyID, ifTrue, ifFalse) = listOf(0, 0, 0)
         var items: List<Item> = listOf()
         var operation = ""
         var divisibleBy = 0L
-        var ifTrue = 0
-        var ifFalse = 0
-        it.lines().forEach { line ->
-            when {
-                line.startsWith("  Operation: ")                  -> operation = line.replace("  Operation: ", "")
-                line.startsWith("  Test: divisible by ")          -> divisibleBy =
-                    line.replace("  Test: divisible by ", "").toLong()
-
-                line.startsWith("    If true: throw to monkey ")  -> ifTrue =
-                    line.replace("    If true: throw to monkey ", "").toInt()
-
-                line.startsWith("    If false: throw to monkey ") -> ifFalse =
-                    line.replace("    If false: throw to monkey ", "").toInt()
-
-                line.startsWith("Monkey ")                        -> monkeyID = line.split(' ').last().trim(':').toInt()
-                line.startsWith("  Starting items: ")             -> items =
-                    line.replace("  Starting items: ", "").split(", ").map { Item(it.toLong()) }
-            }
-        }
+        it.lines().forEach { line -> when {
+            line.startsWith("  Operation: ") -> operation = line.replace("  Operation: ", "")
+            line.startsWith("  Test: divisible by ") -> divisibleBy = line.replace("  Test: divisible by ", "").toLong()
+            line.startsWith("    If true: throw to monkey ") -> ifTrue = line.replace("    If true: throw to monkey ", "").toInt()
+            line.startsWith("    If false: throw to monkey ") -> ifFalse = line.replace("    If false: throw to monkey ", "").toInt()
+            line.startsWith("Monkey ") -> monkeyID = line.split(' ').last().trim(':').toInt()
+            line.startsWith("  Starting items: ") -> items = line.replace("  Starting items: ", "").split(", ").map { Item(it.toLong()) }
+        }}
 
         Monkey(0, monkeyID, items, operation, Test(divisibleBy, ifTrue, ifFalse))
     }
 
     private fun calculateMonkeyBusinessLevel(round: Int, transformer: (Long) -> Long): Long {
-        var copy = monkeys.toMutableList()
+        val copy = monkeys.toMutableList()
         repeat(round) {
             copy.forEachIndexed { id, monkey ->
                 monkey.items.forEach { item ->
@@ -57,7 +40,7 @@ class Day11(input: String) {
                         else -> error("$operation not supported")
                     }
 
-                    item.worryLevel %= copy.map { it.test.divisibleBy.toLong() }.reduce(Long::times)
+                    item.worryLevel %= copy.map { it.test.divisibleBy }.reduce(Long::times)
                     item.worryLevel = transformer(item.worryLevel)
                     monkey.items = monkey.items.drop(1)
                     copy[if (item.worryLevel % monkey.test.divisibleBy == 0L) monkey.test.ifTrue else monkey.test.ifFalse].items += item
