9f057599c4cb2c606b8176ceb67178973d1dcddf
Jefersson Nathan <malukenho.dev@gmail.com>
2022-12-20 21:32:22 +0100

    refactor(12): Day 12 part 1

--- src/main/kotlin/com/github/malukenho/aoc2022/Day12.kt
+++ src/main/kotlin/com/github/malukenho/aoc2022/Day12.kt
@@ -1,41 +1,40 @@
 package com.github.malukenho.aoc2022
 
 class Day12(val input: String) { // Trying to solve it with a BFS
+    private val maze = input.trim().split("\n")
+        .map { xs -> xs.chunked(1).toMutableList() }
+        .toMutableList()
 
     fun part1(): Int {
-        var grid = input.trim().split("\n").map { xs -> xs.chunked(1).toMutableList() }.toMutableList()
+        val grid = maze
 
-        var startRow = 0
-        var startColumn = 0
-        var endRow = 0
-        var endColumn = 0
+        var start = Pair(0, 0)
+        var end = Pair(0, 0)
         grid.forEachIndexed { xsi, xs ->
             xs.forEachIndexed { xzi, xz ->
                 if (xz == "S") {
-                    startRow = xsi
-                    startColumn = xzi
-                    grid[xsi][xzi] = "a"
+                    start = Pair(xsi, xzi)
+                    grid[xsi][xzi] = "a" // I've changed this one just to avoid create a special case for "S"
                 }
                 if (xz == "E") {
-                    endRow = xsi
-                    endColumn = xzi
-                    grid[xsi][xzi] = "z"
+                    end = Pair(xsi, xzi)
+                    grid[xsi][xzi] = "z" // Same here
                 }
             }
         }
 
         // Add values to the queue
         val deque = ArrayDeque<Triple<Int, Int, Int>>()
-        deque.add(Triple(0, startRow, startColumn))
+        deque.add(Triple(0, start.first, start.second))
 
         // We need a visited set as well
         val visited = mutableSetOf<Pair<Int, Int>>()
-        visited.add(Pair(startRow, startColumn))
+        visited.add(Pair(start.first, start.second))
 
         var result = 0
 
         while (deque.isNotEmpty()) {
-            val (d, r, c) = deque.removeFirst()
+            val (distance, r, c) = deque.removeFirst()
             listOf(
                 Pair(r + 1, c), Pair(r - 1, c), Pair(r, c + 1), Pair(r, c - 1)
             ).forEach { t ->
@@ -44,15 +43,15 @@ class Day12(val input: String) { // Trying to solve it with a BFS
 
                 if (nr < 0 || nc < 0 || nr >= grid.size || nc >= grid[0].size) return@forEach
                 if (visited.contains(t)) return@forEach
-                if ((grid[nr][nc].toCharArray()[0].toInt() - grid[r][c].toCharArray()[0].toInt()) > 1) {
+                if ((grid[nr][nc].toCharArray()[0].code - grid[r][c].toCharArray()[0].code) > 1) {
                     return@forEach
                 }
-                if (nr == endRow && nc == endColumn) {
-                    result = d + 1
+                if (nr == end.first && nc == end.second) {
+                    result = distance + 1
                     return@forEach
                 }
                 visited.add(t)
-                deque.add(Triple(d + 1, nr, nc))
+                deque.add(Triple(distance + 1, nr, nc))
             }
         }
 
