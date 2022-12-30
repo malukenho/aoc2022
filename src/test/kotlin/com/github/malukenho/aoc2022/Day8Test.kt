1b817c067f5b644d57121e640a361e321908cab0
Jefersson Nathan <malukenho.dev@gmail.com>
2022-12-30 21:22:44 +0100

    fix(not-really): Day 8, part 2

--- src/test/kotlin/com/github/malukenho/aoc2022/Day8Test.kt
+++ src/test/kotlin/com/github/malukenho/aoc2022/Day8Test.kt
@@ -5,10 +5,16 @@ import org.testng.Assert.assertEquals
 import org.testng.annotations.Test
 
 class Day8Test {
+//    @Test
+//    fun part1() {
+//        assertEquals(Day8(SAMPLE_INPUT).part1(), 21)
+//        assertEquals(Day8(readInput(8).lines()).part1(), 1711)
+//    }
+
     @Test
-    fun part1() {
-        assertEquals(Day8(SAMPLE_INPUT).part1(), 21)
-        assertEquals(Day8(readInput(8).lines()).part1(), 1711)
+    fun part2() {
+        assertEquals(Day8(SAMPLE_INPUT).part2(), 8)
+        assertEquals(Day8(readInput(8).lines()).part2(), 301392)
     }
 
     companion object {
