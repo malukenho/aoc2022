7793f17275e6b20243a08a926a49e6ca0f5ec8d7
Jefersson Nathan <malukenho.dev@gmail.com>
2023-01-24 12:01:07 +0100

    feat: add test cases based on the problem description

--- src/test/kotlin/com/github/malukenho/aoc2022/Day13Test.kt
+++ src/test/kotlin/com/github/malukenho/aoc2022/Day13Test.kt
@@ -1,19 +1,19 @@
 package com.github.malukenho.aoc2022
 
-import Util.readInput
 import org.testng.Assert.assertEquals
 import org.testng.annotations.Test
 
 class Day13Test {
-    private fun String.toIterator() =
-            this.split("""((?<=[\[\],])|(?=[\[\],]))""".toRegex())
-                .filter { it.isNotBlank() }
-                .filter { it != "," }
-                .iterator()
-
     @Test
-    fun parser() {
-        assertEquals(Day13(SAMPLE_INPUT).parser("[[1],4]".toIterator()).toString(), "[[[1], 4]]")
+    fun part1() {
+        assertEquals(Day13(SAMPLE_INPUT).part1("[1,1,3,1,1]", "[1,1,5,1,1]"), Side.RIGHT_ORDER)
+        assertEquals(Day13(SAMPLE_INPUT).part1("[[1],[2,3,4]]", "[[1],4]"), Side.RIGHT_ORDER)
+        assertEquals(Day13(SAMPLE_INPUT).part1("[9]", "[[8,7,6]]"), Side.WRONG_ORDER)
+        assertEquals(Day13(SAMPLE_INPUT).part1("[[4,4],4,4]", "[[4,4],4,4,4]"), Side.RIGHT_ORDER)
+        assertEquals(Day13(SAMPLE_INPUT).part1("[7,7,7,7]", "[7,7,7]"), Side.WRONG_ORDER)
+        assertEquals(Day13(SAMPLE_INPUT).part1("[]", "[3]"), Side.RIGHT_ORDER)
+        assertEquals(Day13(SAMPLE_INPUT).part1("[[[]]]", "[[]]"), Side.WRONG_ORDER)
+//        assertEquals(Day13(SAMPLE_INPUT).part1("[1,[2,[3,[4,[5,6,7]]]],8,9]", "[1,[2,[3,[4,[5,6,0]]]],8,9]"), Side.WRONG_ORDER)
     }
 
     companion object {
