9663e8277df0a8c4c2ef0d11cb0146cf2df59bf9
Jefersson Nathan <malukenho.dev@gmail.com>
2023-01-24 20:40:56 +0100

    feat: refactor solution for day 13

--- src/main/kotlin/com/github/malukenho/aoc2022/Day13.kt
+++ src/main/kotlin/com/github/malukenho/aoc2022/Day13.kt
@@ -5,9 +5,7 @@ enum class Side { RIGHT_ORDER, WRONG_ORDER }
 class Day13(val input: String) {
 
     private fun String.toIterator() =
-        this.split("""((?<=[\[\],])|(?=[\[\],]))""".toRegex())
-            .map { it.trim() }
-            .filter { it.isNotBlank() }
+        this.split("""((?<=[\[\],])|(?=[\[\],]))""".toRegex()).map { it.trim() }.filter { it.isNotBlank() }
             .filter { it != "," }.iterator()
 
     // I had to use an Interator here to maintain the current "pointer"
@@ -26,54 +24,35 @@ class Day13(val input: String) {
         return go(input, mutableListOf())
     }
 
-    fun part1(left: String, right: String): Side {
-        val rightSide = parser(right.toIterator())[0] as List<*>
-        val leftSide = parser(left.toIterator())[0] as List<*>
+    fun compare(right: List<*>, left: List<*>): Side {
+        fun go(index: Int, right: List<*>, left: List<*>): Side {
+            val rightV = right.getOrNull(index)
+            val leftV = left.getOrNull(index)
 
-        println("\n- Compare  $left vs $right")
+            // Draw null
+            if (rightV == null) return if (leftV != null) Side.WRONG_ORDER else Side.RIGHT_ORDER
+            if (leftV == null) return Side.RIGHT_ORDER
+            if (rightV is List<*> && leftV is List<*>) return go(0, rightV, leftV)
+            if (rightV is Int && leftV is List<*>) return go(index, listOf(rightV), leftV)
+            if (rightV is List<*> && leftV is Int) return go(index, rightV, listOf(leftV))
 
-        var index = 0
-        while (true) {
-            var rightV = rightSide.getOrNull(index)
-            var leftV = leftSide.getOrNull(index)
-
-            if (rightV == null) {
-                return if (leftV != null) {
-                    Side.WRONG_ORDER
-                } else {
-                    Side.RIGHT_ORDER
-                }
-            }
-
-            var rightLoop = 0
-            while (rightV is List<*>) {
-                rightLoop++
-                println("    - Compare $leftV vs $rightV")
-                rightV = rightV.getOrNull(0)
-            }
-
-            var leftLoop = 0
-            while (leftV is List<*>) {
-                leftLoop++
-                println("    - Compare $leftV vs $rightV")
-                leftV = leftV.getOrNull(0)
-            }
-
-            if (rightV == null && leftLoop > rightLoop) {
-                return Side.WRONG_ORDER
-            }
-
-            if (rightV is Int && leftV is Int) {
-                println("    - Compare $leftV vs $rightV")
-                if (leftV > rightV) {
-                    println("        - Left side is GREATER, so inputs are in the WRONG order")
-                    return Side.WRONG_ORDER
-                }
-            }
-
-            index++
+            return if (leftV as Int > rightV as Int) Side.WRONG_ORDER else go(index+1, right, left)
         }
 
-        @Suppress("UNREACHABLE_CODE") return Side.RIGHT_ORDER
+        return go(0, right, left)
+    }
+
+    fun part1(): Int {
+        val pairs = input.split("\n\n").map { it.split("\n") }
+
+        var counter = 0
+        pairs.forEachIndexed { index, xs ->
+            val (left, right) = xs
+            if (compare(parser(right.toIterator()), parser(left.toIterator())) == Side.RIGHT_ORDER) {
+                counter += index + 1
+            }
+        }
+
+        return counter
     }
 }
--- src/test/kotlin/com/github/malukenho/aoc2022/Day13Test.kt
+++ src/test/kotlin/com/github/malukenho/aoc2022/Day13Test.kt
@@ -1,19 +1,37 @@
 package com.github.malukenho.aoc2022
 
+import Util.readInput
 import org.testng.Assert.assertEquals
 import org.testng.annotations.Test
 
 class Day13Test {
+    private fun String.toIterator() =
+        this.split("""((?<=[\[\],])|(?=[\[\],]))""".toRegex()).map { it.trim() }.filter { it.isNotBlank() }
+            .filter { it != "," }.iterator()
+
+    @Test
+    fun compare() {
+        assertEquals(callCompare("[1,1,3,1,1]", "[1,1,5,1,1]"), Side.RIGHT_ORDER)
+        assertEquals(callCompare("[[1],[2,3,4]]", "[[1],4]"), Side.RIGHT_ORDER)
+        assertEquals(callCompare("[9]", "[[8,7,6]]"), Side.WRONG_ORDER)
+        assertEquals(callCompare("[[4,4],4,4]", "[[4,4],4,4,4]"), Side.RIGHT_ORDER)
+        assertEquals(callCompare("[7,7,7,7]", "[7,7,7]"), Side.WRONG_ORDER)
+        assertEquals(callCompare("[]", "[3]"), Side.RIGHT_ORDER)
+        assertEquals(callCompare("[[[]]]", "[[]]"), Side.WRONG_ORDER)
+        assertEquals(
+            callCompare("[1,[2,[3,[4,[5,6,7]]]],8,9]", "[1,[2,[3,[4,[5,6,0]]]],8,9]"), Side.WRONG_ORDER
+        )
+    }
+
+    private fun callCompare(left: String, right: String): Side {
+        val s = Day13(SAMPLE_INPUT)
+        return s.compare(s.parser(right.toIterator()), s.parser(left.toIterator()))
+    }
+
     @Test
     fun part1() {
-        assertEquals(Day13(SAMPLE_INPUT).part1("[1,1,3,1,1]", "[1,1,5,1,1]"), Side.RIGHT_ORDER)
-        assertEquals(Day13(SAMPLE_INPUT).part1("[[1],[2,3,4]]", "[[1],4]"), Side.RIGHT_ORDER)
-        assertEquals(Day13(SAMPLE_INPUT).part1("[9]", "[[8,7,6]]"), Side.WRONG_ORDER)
-        assertEquals(Day13(SAMPLE_INPUT).part1("[[4,4],4,4]", "[[4,4],4,4,4]"), Side.RIGHT_ORDER)
-        assertEquals(Day13(SAMPLE_INPUT).part1("[7,7,7,7]", "[7,7,7]"), Side.WRONG_ORDER)
-        assertEquals(Day13(SAMPLE_INPUT).part1("[]", "[3]"), Side.RIGHT_ORDER)
-        assertEquals(Day13(SAMPLE_INPUT).part1("[[[]]]", "[[]]"), Side.WRONG_ORDER)
-//        assertEquals(Day13(SAMPLE_INPUT).part1("[1,[2,[3,[4,[5,6,7]]]],8,9]", "[1,[2,[3,[4,[5,6,0]]]],8,9]"), Side.WRONG_ORDER)
+        assertEquals(Day13(SAMPLE_INPUT).part1(), 13)
+        assertEquals(Day13(readInput(13)).part1(), 0)
     }
 
     companion object {
