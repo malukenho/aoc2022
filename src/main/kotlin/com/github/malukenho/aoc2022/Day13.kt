cd2e83877dbc66bcc17d3012da36cfc99dcc9920
Jefersson Nathan <malukenho.dev@gmail.com>
2023-01-24 22:40:16 +0100

    feat: day 13 part 2

--- src/main/kotlin/com/github/malukenho/aoc2022/Day13.kt
+++ src/main/kotlin/com/github/malukenho/aoc2022/Day13.kt
@@ -1,58 +1,51 @@
 package com.github.malukenho.aoc2022
 
-enum class Side { RIGHT_ORDER, WRONG_ORDER }
-
 class Day13(val input: String) {
 
-    private fun String.toIterator() =
-        this.split("""((?<=[\[\],])|(?=[\[\],]))""".toRegex()).map { it.trim() }.filter { it.isNotBlank() }
-            .filter { it != "," }.iterator()
+    private val lists = input.split("\n")
+        .filter { !it.isEmpty() }
+        .map { it.parseLists() }
+        .toMutableList()
 
-    // I had to use an Interator here to maintain the current "pointer"
-    // state during the recursive calls otherwise it was getting
-    // impossible to solve deep level list
-    // TODO: We can parse it as a JSON !!!!!!!!!!!!!!!!!!!!!!!
-    tailrec fun parser(input: Iterator<String>): List<Any> {
-        fun go(c: Iterator<String>, acc: MutableList<Any>): MutableList<Any> {
-            if (!c.hasNext()) return acc
-            val s = c.next()
-            if (s == "]") return acc
-            if (s == "[") acc.add(go(c, mutableListOf()))
-            if (s.toCharArray().first().isDigit()) acc.add(s.toInt())
-            return go(c, acc)
+    fun String.parseLists(): Any {
+        val stack: MutableList<MutableList<Any>> = mutableListOf(mutableListOf())
+        this.replace("]", ",}").replace("[", "{,").replace(",,", ",").split(",").forEach {
+            when (it) {
+                "{" -> { val m: MutableList<Any> = mutableListOf(); stack.last().add(m); stack.add(m) }
+                "}" -> stack.removeLast()
+                else -> stack.last().add(it.toInt())
+            }
         }
-        return go(input, mutableListOf())
+        return stack[0][0]
     }
 
-    fun compare(right: List<*>, left: List<*>): Side {
-        fun go(index: Int, right: List<*>, left: List<*>): Side {
-            val rightV = right.getOrNull(index)
-            val leftV = left.getOrNull(index)
-
-            // Draw null
-            if (rightV == null) return if (leftV != null) Side.WRONG_ORDER else Side.RIGHT_ORDER
-            if (leftV == null) return Side.RIGHT_ORDER
-            if (rightV is List<*> && leftV is List<*>) return go(0, rightV, leftV)
-            if (rightV is Int && leftV is List<*>) return go(index, listOf(rightV), leftV)
-            if (rightV is List<*> && leftV is Int) return go(index, rightV, listOf(leftV))
-
-            return if (leftV as Int > rightV as Int) Side.WRONG_ORDER else go(index+1, right, left)
-        }
-
-        return go(0, right, left)
+    fun cmp(a: Any, b: Any): Int {
+        if (a is Int && b is Int)
+            return when { a < b -> -1; a > b -> 1; else -> 0 }
+        val aList = if (a is MutableList<*>) a else mutableListOf(a)
+        val bList = if (b is MutableList<*>) b else mutableListOf(b)
+        for ((u, v) in aList zip bList)
+            if (cmp(u!!, v!!) != 0)
+                return cmp(u, v)
+        return cmp(aList.size, bList.size)
     }
 
     fun part1(): Int {
-        val pairs = input.split("\n\n").map { it.split("\n") }
+        return lists
+            .chunked(2)
+            .withIndex()
+            .filter { (_, pair) -> cmp(pair[0], pair[1]) <= 0 }
+            .map { (i, _) -> i+1 }
+            .sum()
+    }
 
-        var counter = 0
-        pairs.forEachIndexed { index, xs ->
-            val (left, right) = xs
-            if (compare(parser(right.toIterator()), parser(left.toIterator())) == Side.RIGHT_ORDER) {
-                counter += index + 1
-            }
-        }
-
-        return counter
+    fun part2(): Int {
+        val distress = listOf(listOf(listOf(2)), listOf(listOf(6)))
+        return lists
+            .also { it.addAll(distress) }
+            .sortedWith(::cmp)
+            .withIndex()
+            .filter { it.value in distress }
+            .fold(1) { acc, (i, _) -> (i+1) * acc }
     }
 }
--- src/test/kotlin/com/github/malukenho/aoc2022/Day13Test.kt
+++ src/test/kotlin/com/github/malukenho/aoc2022/Day13Test.kt
@@ -5,33 +5,17 @@ import org.testng.Assert.assertEquals
 import org.testng.annotations.Test
 
 class Day13Test {
-    private fun String.toIterator() =
-        this.split("""((?<=[\[\],])|(?=[\[\],]))""".toRegex()).map { it.trim() }.filter { it.isNotBlank() }
-            .filter { it != "," }.iterator()
-
-    @Test
-    fun compare() {
-        assertEquals(callCompare("[1,1,3,1,1]", "[1,1,5,1,1]"), Side.RIGHT_ORDER)
-        assertEquals(callCompare("[[1],[2,3,4]]", "[[1],4]"), Side.RIGHT_ORDER)
-        assertEquals(callCompare("[9]", "[[8,7,6]]"), Side.WRONG_ORDER)
-        assertEquals(callCompare("[[4,4],4,4]", "[[4,4],4,4,4]"), Side.RIGHT_ORDER)
-        assertEquals(callCompare("[7,7,7,7]", "[7,7,7]"), Side.WRONG_ORDER)
-        assertEquals(callCompare("[]", "[3]"), Side.RIGHT_ORDER)
-        assertEquals(callCompare("[[[]]]", "[[]]"), Side.WRONG_ORDER)
-        assertEquals(
-            callCompare("[1,[2,[3,[4,[5,6,7]]]],8,9]", "[1,[2,[3,[4,[5,6,0]]]],8,9]"), Side.WRONG_ORDER
-        )
-    }
-
-    private fun callCompare(left: String, right: String): Side {
-        val s = Day13(SAMPLE_INPUT)
-        return s.compare(s.parser(right.toIterator()), s.parser(left.toIterator()))
-    }
 
     @Test
     fun part1() {
         assertEquals(Day13(SAMPLE_INPUT).part1(), 13)
-        assertEquals(Day13(readInput(13)).part1(), 0)
+        assertEquals(Day13(readInput(13)).part1(), 5659)
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(Day13(SAMPLE_INPUT).part2(), 140)
+        assertEquals(Day13(readInput(13)).part2(), 22110)
     }
 
     companion object {
