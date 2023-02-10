1f5746714cbfe55aa79f7c9ac8cc45baa967bf50
Jefersson Nathan <malukenho.dev@gmail.com>
2023-02-10 02:02:29 +0100

    playground: refactor and simplify floodFill implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,28 +10,23 @@ fun main() {
         ".......▓▓▓▓...▓▓▓▓▓▓▓................".split("").toMutableList(),
     )
 
-    fun paint(image: Array<MutableList<String>>, x: Int, y: Int, symbol: String): Array<MutableList<String>> {
-        if (image.getOrNull(x)?.getOrNull(y) == null || image[x][y] == symbol || image[x][y] == "▓") {
+    fun floodFill(image: Array<MutableList<String>>, coords: Pair<Int, Int>, fill: String): Array<MutableList<String>> {
+        val (x, y) = coords
+
+        if (image.getOrNull(x)?.getOrNull(y) == null || image[x][y] == fill || image[x][y] == "▓") {
             return image
         }
 
-        image[x][y] = symbol;
+        image[x][y] = fill;
 
-        var imagem = paint(image, x, y - 1, symbol)
-        imagem = paint(imagem, x, y + 1, symbol)
-        imagem = paint(imagem, x + 1, y, symbol)
-        imagem = paint(imagem, x - 1, y, symbol)
+        floodFill(image, x to (y - 1), fill)
+        floodFill(image, x to (y + 1), fill)
+        floodFill(image, (x + 1) to y, fill)
+        floodFill(image, (x - 1) to y, fill)
 
-        return imagem
+        return image
     }
 
-    paint(image, 3, 16, "░").joinToString("\n") { it.joinToString("") }.also(::println)
-
-    println("\n\n")
-
-//    paint(image, 6, 3, "░").joinToString("\n") { it.joinToString("") }.also(::println)
-//
-//    println("\n\n")
-//
-//    paint(image, 1, 7, "░").joinToString("\n") { it.joinToString("") }.also(::println)
+    val filledImage = floodFill(image, 0 to 29, "░")
+    println(filledImage.joinToString("\n") { it.joinToString("") })
 }
