ce0b7afbf8b2eedd72155158a99647ce301a21e8
Jefersson Nathan <malukenho.dev@gmail.com>
2023-03-03 14:53:10 +0100

    playground: defining a Monoid

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,31 @@
+interface Monoid<T> {
+    val unit: T
+    val combine: (T, T) -> T
+}
+
 fun main() {
+
+     fun <A, B, C> (A.(B) -> C).swap(): (B.(A) -> C) = { a: A -> //1
+         a.this@swap(this)
+     }
+
+    fun <T> Monoid<T>.commutate(): Monoid<T> = object: Monoid<T> {
+        override val unit: T
+            get() = this@commutate.unit
+        override val combine: T.(T) -> T
+            get() = this@commutate.combine.swap()
+    }
+
+     fun concat(l: List<String>): String = if (l.isEmpty()) {
+         ""
+     } else {
+         l.first() + concat(l.dropWhile { it == l.first() })
+     }
+
+    concat(listOf("He", "l", "lo", " Wor", "ld!")).also(::println)
+
+    fun countRooms = TODO("Create an algo to count rooms in a maze.")
+
     val image = arrayOf(
         "..▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓...........".split("").toMutableList(),
         "..▓......................▓...▓▓▓▓▓...".split("").toMutableList(),
@@ -28,5 +55,6 @@ fun main() {
     }
 
     val filledImage = floodFill(image, 0 to 29, "░")
+
     println(filledImage.joinToString("\n") { it.joinToString("") })
 }
