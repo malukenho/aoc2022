6838de3e57a900815fd6e5934b625bff6681e57c
Jefersson Nathan <malukenho.dev@gmail.com>
2023-03-24 15:30:32 +0100

    feat(day-14): clean-up

--- src/main/kotlin/com/github/malukenho/aoc2022/Day14.kt
+++ src/main/kotlin/com/github/malukenho/aoc2022/Day14.kt
@@ -1,16 +1,5 @@
 package com.github.malukenho.aoc2022
 
-//val p: Process<ForIO, String> =
-//    Await<ForIO, BufferedReader, String>(
-//        IO { BufferedReader(FileReader(fileName))}
-//    ) { ei1: Either<Throwable, BufferedReader> ->
-//        when (ei1) {
-//            is Right -> processNext(ei1)
-//            is Left -> Halt(ei1.value)
-//        }
-//
-//    }
-
 typealias Rocks = Pair<Int, Int>
 typealias CoordinateList = MutableList<MutableList<Rocks>>
 
@@ -20,6 +9,7 @@ fun Cave.getXY(x: Int, y: Int) = this.getOrNull(x)?.getOrNull(y)
 
 fun CoordinateList.max() = this.flatMap { map -> map.map { it.first } }.max()
 fun CoordinateList.min() = this.flatMap { map -> map.map { it.first } }.min()
+fun CoordinateList.rowsRange() = 0 .. this.flatMap { map -> map.map { it.second } }.max()
 
 class Day14(val input: String) {
 
@@ -31,19 +21,22 @@ class Day14(val input: String) {
         }.toMutableList()
     }.toMutableList()
 
+    private val columns: IntRange = routes.min()..routes.max()
+
     private fun MutableList<MutableList<Pair<Int, String>>>.show() = this.forEach {
         println(it.joinToString("") { that -> that.second })
     }
 
     fun part1(): Int {
         // @TODO("Which name better reflects the problem proposed domain?")
-        val columns: IntRange = routes.min()..routes.max()
 
-        var cave: Cave = columns.map {
+        println(routes.rowsRange())
+        println(columns)
+        // Create a cave
+        var cave: Cave = routes.rowsRange().map {
             columns.map { column -> Pair(column, ".") }.toMutableList()
         }.toMutableList()
 
-        cave.show()
         routes.forEach { coordinates ->
             coordinates.windowed(2, 1, false).forEach {
                 val first = it.first()
@@ -77,22 +70,19 @@ class Day14(val input: String) {
 
         val source = 500
         var iterations = 0
-        fun dropSand(cave: Cave, fallingX: Int, fallingY: Int, initial: Int): Cave {
+        tailrec fun dropSand(cave: Cave, fallingX: Int, fallingY: Int, initial: Int): Cave {
 
             val currentChar = cave.getXY(fallingX, fallingY)?.second
 
             if (currentChar == null) {
-                println("reset")
                 return dropSand(cave, 0, initial, initial)
             }
 
             if (0 == fallingX && initial == fallingY && currentChar == "o") {
-                println("oxe")
                 return cave
             }
 
             if (currentChar == ".") {
-                println("down we  go")
                 return dropSand(cave, fallingX+1, fallingY, initial)
             }
 
@@ -114,31 +104,25 @@ class Day14(val input: String) {
                 }
 
                 if (downLeftChar == "o" && downRightChar == "o") {
-                    println("rest - $currentChar")
                     cave.get(fallingX - 1).set(fallingY, Pair(source, "o"))
 
                     iterations++
-                    cave.show()
                     return dropSand(cave, 0, initial, initial)
                 }
 
                 if (downLeftChar == "#" && downRightChar == "#") {
-                    println("rest - $currentChar")
                     cave.get(fallingX - 1).set(fallingY, Pair(source, "o"))
 
                     iterations++
-                    cave.show()
                     return dropSand(cave, 0, initial, initial)
                 }
 
                 // look left
                 if (downLeftChar == "o" && cave.getXY(fallingX-1, fallingY - 1)?.second == ".") {
-                    println("rest - downLeft o")
                     return dropSand(cave, fallingX, fallingY-1, initial)
                 }
 
                 if (downLeftChar == "#" && cave.getXY(fallingX+1, fallingY - 1)?.second == ".") {
-                   println("rest - downLeft #")
                    return dropSand(cave, fallingX, fallingY-1, initial)
                 }
 
@@ -147,8 +131,6 @@ class Day14(val input: String) {
                     cave.get(fallingX - 1).set(fallingY, Pair(source, "o"))
 
                     iterations++
-                    println("rest - downRight o")
-                    cave.show()
                     return dropSand(cave, 0, initial, initial)
                 }
 
@@ -157,27 +139,22 @@ class Day14(val input: String) {
 
                     iterations++
                     println("rest - downRight #")
-                    cave.show()
                     return dropSand(cave, 0, initial, initial)
                 }
             }
 
             if (currentChar == "#" || currentChar == "o") {
                 if (cave.getXY(fallingX, fallingY - 1)?.second == ".") {
-                    println("skip left")
                     return dropSand(cave, fallingX, fallingY - 1, initial)
                 }
 
                 if (cave.getXY(fallingX, fallingY + 1)?.second == ".") {
-                    println("skip right")
                     return dropSand(cave, fallingX, fallingY + 1, initial)
                 }
 
-                println("rest - normal $currentChar")
                 cave.get(fallingX - 1).set(fallingY, Pair(source, "o"))
 
                 iterations++
-                cave.show()
                 return dropSand(cave, 0, initial, initial)
             }
 
