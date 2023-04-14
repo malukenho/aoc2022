a0f379806cd944a14211b5a58dc5a3e4bd7770f7
Jefersson Nathan <malukenho.dev@gmail.com>
2023-04-14 20:58:06 +0200

    feat: Add maze function to Main.kt file
    The maze function creates a maze using a multiline string and prints it to the console. The maze is represented using different characters, including walls (▓), empty spaces ( ), and start (S) and end (E) points. The function currently does not include any logic for solving the maze.
    
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,56 +5,88 @@ interface Monoid<T> {
 
 fun main() {
 
-     fun <A, B, C> (A.(B) -> C).swap(): (B.(A) -> C) = { a: A -> //1
-         a.this@swap(this)
-     }
+    maze()
+    return;
+//     fun <A, B, C> (A.(B) -> C).swap(): (B.(A) -> C) = { a: A -> //1
+//         a.this@swap(this)
+//     }
+//
+//    fun <T> Monoid<T>.commutate(): Monoid<T> = object: Monoid<T> {
+//        override val unit: T
+//            get() = this@commutate.unit
+//        override val combine: T.(T) -> T
+//            get() = this@commutate.combine.swap()
+//    }
+//
+//     fun concat(l: List<String>): String = if (l.isEmpty()) {
+//         ""
+//     } else {
+//         l.first() + concat(l.dropWhile { it == l.first() })
+//     }
+//
+//    concat(listOf("He", "l", "lo", " Wor", "ld!")).also(::println)
+//
+//    fun countRooms(): Nothing = TODO("Create an algo to count rooms in a maze.")
+//
+//    val image = arrayOf(
+//        "..▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓...........".split("").toMutableList(),
+//        "..▓......................▓...▓▓▓▓▓...".split("").toMutableList(),
+//        "..▓..........▓▓▓▓▓▓▓▓....▓▓▓▓▓...▓...".split("").toMutableList(),
+//        "..▓..........▓......▓............▓...".split("").toMutableList(),
+//        "..▓..........▓▓▓▓▓▓▓▓.........▓▓▓▓...".split("").toMutableList(),
+//        "..▓▓▓▓▓▓......................▓......".split("").toMutableList(),
+//        ".......▓..▓▓▓▓▓.....▓▓▓▓▓▓▓▓▓▓▓......".split("").toMutableList(),
+//        ".......▓▓▓▓...▓▓▓▓▓▓▓................".split("").toMutableList(),
+//    )
+//
+//    fun floodFill(image: Array<MutableList<String>>, coords: Pair<Int, Int>, fill: String): Array<MutableList<String>> {
+//        val (x, y) = coords
+//
+//        if (image.getOrNull(x)?.getOrNull(y) == null || image[x][y] == fill || image[x][y] == "▓") {
+//            return image
+//        }
+//
+//        image[x][y] = fill;
+//
+//        floodFill(image, x to (y - 1), fill)
+//        floodFill(image, x to (y + 1), fill)
+//        floodFill(image, (x + 1) to y, fill)
+//        floodFill(image, (x - 1) to y, fill)
+//
+//        return image
+//    }
+//
+//    val filledImage = floodFill(image, 0 to 29, "░")
+//
+//    println(filledImage.joinToString("\n") { it.joinToString("") })
+}
 
-    fun <T> Monoid<T>.commutate(): Monoid<T> = object: Monoid<T> {
-        override val unit: T
-            get() = this@commutate.unit
-        override val combine: T.(T) -> T
-            get() = this@commutate.combine.swap()
-    }
+fun maze() {
+    val maze =
+        """
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓S▓                 ▓       ▓ ▓   ▓     ▓         ▓     ▓   ▓         ▓
+▓ ▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓ ▓ ▓▓▓ ▓▓▓ ▓ ▓ ▓ ▓ ▓▓▓ ▓ ▓ ▓▓▓▓▓ ▓ ▓▓▓ ▓ ▓ ▓▓▓▓▓ ▓ ▓▓▓
+▓ ▓   ▓     ▓     ▓     ▓   ▓ ▓ ▓   ▓ ▓   ▓ ▓       ▓ ▓ ▓ ▓     ▓ ▓   ▓
+▓ ▓ ▓ ▓▓▓▓▓ ▓ ▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓ ▓ ▓▓▓▓▓ ▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓ ▓ ▓ ▓▓▓▓▓ ▓▓▓ ▓ ▓
+▓   ▓     ▓ ▓ ▓     ▓   ▓   ▓   ▓         ▓       ▓   ▓   ▓   ▓   ▓ ▓ ▓
+▓▓▓▓▓▓▓▓▓ ▓ ▓ ▓ ▓▓▓▓▓ ▓ ▓▓▓ ▓ ▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓ ▓ ▓ ▓▓▓▓▓ ▓▓▓▓▓ ▓▓▓ ▓
+▓       ▓ ▓ ▓ ▓     ▓ ▓     ▓ ▓   ▓   ▓   ▓     ▓ ▓ ▓   ▓         ▓   ▓
+▓ ▓ ▓▓▓▓▓ ▓ ▓ ▓▓▓ ▓ ▓ ▓▓▓▓▓▓▓ ▓ ▓ ▓ ▓ ▓ ▓ ▓ ▓▓▓▓▓ ▓▓▓ ▓▓▓ ▓▓▓▓▓▓▓▓▓ ▓ ▓
+▓ ▓ ▓   ▓ ▓ ▓   ▓ ▓ ▓     ▓     ▓   ▓   ▓   ▓   ▓   ▓     ▓         ▓ ▓
+▓▓▓ ▓ ▓ ▓ ▓ ▓▓▓ ▓ ▓ ▓▓▓▓▓ ▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓ ▓ ▓▓▓ ▓ ▓▓▓▓▓ ▓▓▓▓▓ ▓▓▓ ▓
+▓   ▓ ▓   ▓ ▓   ▓ ▓     ▓   ▓     ▓       ▓   ▓     ▓ ▓     ▓     ▓   ▓
+▓ ▓▓▓ ▓▓▓▓▓▓▓ ▓▓▓▓▓ ▓▓▓ ▓▓▓ ▓▓▓▓▓▓▓ ▓▓▓▓▓ ▓ ▓▓▓▓▓▓▓▓▓ ▓▓▓ ▓▓▓ ▓▓▓▓▓ ▓▓▓
+▓   ▓         ▓     ▓     ▓       ▓   ▓ ▓   ▓ ▓     ▓   ▓ ▓   ▓ ▓   ▓ ▓
+▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓ ▓ ▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓ ▓▓▓ ▓ ▓▓▓▓▓ ▓ ▓ ▓▓▓▓▓ ▓ ▓ ▓▓▓ ▓ ▓▓▓ ▓
+▓   ▓   ▓       ▓ ▓     ▓   ▓   ▓     ▓       ▓ ▓ ▓     ▓ ▓ ▓   ▓ ▓   ▓
+▓ ▓▓▓ ▓ ▓ ▓▓▓▓▓▓▓ ▓ ▓▓▓ ▓▓▓▓▓ ▓ ▓▓▓▓▓▓▓ ▓▓▓ ▓▓▓ ▓ ▓ ▓▓▓▓▓▓▓ ▓ ▓ ▓ ▓▓▓ ▓
+▓     ▓         ▓     ▓       ▓           ▓     ▓           ▓ ▓      E▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+        """.split("\n")
 
-     fun concat(l: List<String>): String = if (l.isEmpty()) {
-         ""
-     } else {
-         l.first() + concat(l.dropWhile { it == l.first() })
-     }
 
-    concat(listOf("He", "l", "lo", " Wor", "ld!")).also(::println)
+    // height of the maze
 
-    fun countRooms(): Nothing = TODO("Create an algo to count rooms in a maze.")
 
-    val image = arrayOf(
-        "..▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓...........".split("").toMutableList(),
-        "..▓......................▓...▓▓▓▓▓...".split("").toMutableList(),
-        "..▓..........▓▓▓▓▓▓▓▓....▓▓▓▓▓...▓...".split("").toMutableList(),
-        "..▓..........▓......▓............▓...".split("").toMutableList(),
-        "..▓..........▓▓▓▓▓▓▓▓.........▓▓▓▓...".split("").toMutableList(),
-        "..▓▓▓▓▓▓......................▓......".split("").toMutableList(),
-        ".......▓..▓▓▓▓▓.....▓▓▓▓▓▓▓▓▓▓▓......".split("").toMutableList(),
-        ".......▓▓▓▓...▓▓▓▓▓▓▓................".split("").toMutableList(),
-    )
-
-    fun floodFill(image: Array<MutableList<String>>, coords: Pair<Int, Int>, fill: String): Array<MutableList<String>> {
-        val (x, y) = coords
-
-        if (image.getOrNull(x)?.getOrNull(y) == null || image[x][y] == fill || image[x][y] == "▓") {
-            return image
-        }
-
-        image[x][y] = fill;
-
-        floodFill(image, x to (y - 1), fill)
-        floodFill(image, x to (y + 1), fill)
-        floodFill(image, (x + 1) to y, fill)
-        floodFill(image, (x - 1) to y, fill)
-
-        return image
-    }
-
-    val filledImage = floodFill(image, 0 to 29, "░")
-
-    println(filledImage.joinToString("\n") { it.joinToString("") })
 }
